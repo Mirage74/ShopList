39238e257cd121fa94fe48fd32d201264d1c374d
balex <balexvicx@gmail.com>
2024-03-11 00:24:52 +0100

    lessin 13

--- app/src/main/java/com/example/shoplist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoplist/domain/GetShopItemUseCase.kt
@@ -1,8 +1,5 @@
 package com.example.shoplist.domain
 
-import com.example.shoplist.domain.ShopItem
-import com.example.shoplist.domain.ShopListRepository
-
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun getShopItem(shopItemId: Int): ShopItem {
